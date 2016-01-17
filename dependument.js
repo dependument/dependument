ad5aeb06925d8d9c4872d7690500944924ee08fa
James Monger <jameskmonger@hotmail.co.uk>
2016-01-17 00:00:35 +0000

    Read the templates from the directory

--- dependument.js
+++ dependument.js
@@ -5,14 +5,22 @@
 
   const fs = require('fs');
   const CONFIG_FILE = "package.json";
+  const templates = loadTemplates(__dirname + "/templates");
 
   (function() {
     let file = readFile(CONFIG_FILE);
 
     let dependencies = getDependencies(file);
-
-    console.log(dependencies);
   })();
+
+  function loadTemplates(path) {
+    let files = fs.readdirSync(path);
+    let output = {};
+
+    for (let f of files) {
+      console.log(f);
+    }
+  }
 
   function getDependencies(file) {
     let dependencies = file.dependencies;
