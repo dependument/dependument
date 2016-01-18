b5d245d9f5abcac7475ff0a30182f83f093ed827
James Monger <jameskmonger@hotmail.co.uk>
2016-01-18 12:38:50 +0000

    Create gulpfile.ts

---
+++ gulpfile.ts
@@ -0,0 +1,24 @@
+/// <reference path="typings/requirejs/require.d.ts" />
+
+declare var __dirname: string;
+
+class GulpEnvironment {
+  private const gulp;
+  private const tsc;
+  private const Server;
+  private const srcTSConfig;
+  private const specTSConfig;
+
+  constructor() {
+    this.gulp = require('gulp');
+    this.tsc = require('gulp-typescript');
+    this.Server = require('karma').Server;
+
+    this.srcTSConfig = tsc.createProject('src/tsconfig.json');
+    this.specTSConfig = tsc.createProject('spec/tsconfig.json');
+  }
+
+  registerTasks() {
+
+  }
+}
