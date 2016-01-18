c541c524f6ab397e06bf3a0e21c75f1398313f91
James Monger <jameskmonger@hotmail.co.uk>
2016-01-18 12:44:53 +0000

    Typescript gulp environment setup

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,23 +3,24 @@
 declare var __dirname: string;
 
 class GulpEnvironment {
-  private const gulp;
-  private const tsc;
-  private const Server;
-  private const srcTSConfig;
-  private const specTSConfig;
+  private gulp;
+  private tsc;
+  private Server;
+  private srcTSConfig;
+  private specTSConfig;
 
   constructor() {
     this.gulp = require('gulp');
     this.tsc = require('gulp-typescript');
     this.Server = require('karma').Server;
-
-    this.srcTSConfig = tsc.createProject('src/tsconfig.json');
-    this.specTSConfig = tsc.createProject('spec/tsconfig.json');
   }
 
   registerTasks() {
     let gulp = this.gulp;
+
+    gulp.task('bla', () => {
+      console.log("bla bla gulp is working");
+    });
   }
 }
 
