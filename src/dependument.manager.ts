8f872872e8e331083472b19c6f3b1573a9574286
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 13:56:35 +0000

    Inject a templatefilesystem into dependumentmanager

--- src/dependument.manager.ts
+++ src/dependument.manager.ts
@@ -1,13 +1,16 @@
 import { IFileSystem } from './filesystem/filesystem.i';
+import { ITemplateFileSystem } from './templates/templatefilesystem.i';
 import * as Path from 'path';
 
 export class DependumentManager {
   private _fileSystem : IFileSystem;
+  private _templateFileSystem : ITemplateFileSystem;
 
-  constructor(fileSystem: IFileSystem) {
+  constructor(fileSystem: IFileSystem, templateFileSystem: ITemplateFileSystem) {
     let rootName = Path.resolve(__dirname, '../');
 
     this._fileSystem = fileSystem;
+    this._templateFileSystem = templateFileSystem;
 
     console.log(this._fileSystem.getDependencies(rootName + "/package.json"));
     console.log(this._fileSystem.getDevDependencies(rootName + "/package.json"));
