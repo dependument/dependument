b326b81d845f1adbe5a05370fd840bd21942ea0c
James Monger <jameskmonger@hotmail.co.uk>
2016-01-21 14:07:14 +0000

    Create options object in DependumentManager constructor

--- src/dependument.manager.ts
+++ src/dependument.manager.ts
@@ -1,13 +1,22 @@
 import { IFileSystem } from './filesystem/filesystem.i';
 import { ITemplateFileSystem } from './templates/templatefilesystem.i';
+import { IOptions } from './options/options.i';
 import * as Path from 'path';
 
 export class DependumentManager {
   private _fileSystem : IFileSystem;
   private _templateFileSystem : ITemplateFileSystem;
+  public options: IOptions;
 
   constructor(fileSystem: IFileSystem, templateFileSystem: ITemplateFileSystem) {
     this._fileSystem = fileSystem;
     this._templateFileSystem = templateFileSystem;
+
+    this.options = {
+      templates: {
+        dependencyTemplateFile: "dependency.md",
+        outputTemplateFile: "output.md"
+      }
+    };
   }
 }
