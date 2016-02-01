015b85a7b43036fde37d2cc33498aaeeefe6d8c0
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-01 13:08:41 +0000

    Remove all code

--- src/filesystem/filesystem.i.ts
+++
@@ -1,6 +0,0 @@
-export interface IFileSystem {
-  canReadPackageInfo(directory: string): boolean;
-  getDependencies(path: string): any;
-  getDevDependencies(path: string): any;
-  writeDependencies(dependencies: any): any;
-}
