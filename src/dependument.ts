2183ae10f054769b05a8b143a9140c7eefcd5713
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-02 13:07:21 +0000

    Write the output nicely

--- src/dependument.ts
+++ src/dependument.ts
@@ -6,6 +6,8 @@ export class Dependument {
   private source: string;
   private output: string;
 
+  private static DEPENCENCY_TEMPLATE: string = "* [{{package_name}}]({{package_url}}) ({{package_version}})\n";
+
   constructor(options: any) {
     if (!options) {
       throw new Error("No options provided");
@@ -39,15 +41,46 @@ export class Dependument {
 
       let deps: string[][] = [];
 
-      deps["dependencies"] = json["dependencies"].toString();
-      deps["devDependencies"] = json["devDependencies"].toString();
+      deps["dependencies"] = json["dependencies"];
+      deps["devDependencies"] = json["devDependencies"];
 
       success(deps);
     });
   }
 
   private writeDependencies(deps: string[][]) {
-    let output = `${deps["dependencies"]}\n${deps["devDependencies"]}`;
+    let output = "";
+
+    output += "## Dependencies\n";
+    output += "### For users\n";
+
+    Object.keys(deps["dependencies"]).forEach(function (key) {
+      let name = key;
+      let version = deps["dependencies"][key];
+      let url = "https://www.npmjs.com/package/" + name;
+
+      let line = Dependument.DEPENCENCY_TEMPLATE
+                  .replace("{{package_name}}", name)
+                  .replace("{{package_url}}", url)
+                  .replace("{{package_version}}", version);
+
+      output += line;
+    });
+
+    output += "### For developers\n";
+
+    Object.keys(deps["devDependencies"]).forEach(function (key) {
+      let name = key;
+      let version = deps["devDependencies"][key];
+      let url = "https://www.npmjs.com/package/" + name;
+
+      let line = Dependument.DEPENCENCY_TEMPLATE
+                  .replace("{{package_name}}", name)
+                  .replace("{{package_url}}", url)
+                  .replace("{{package_version}}", version);
+
+      output += line;
+    });
 
     fs.writeFile(this.output, output, (err) => {
       if (err) throw err;
