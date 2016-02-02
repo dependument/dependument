1430ec431c9451549d293dd320c487a2895c7e34
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-02 18:34:25 +0000

    Create getDependenciesOutput

--- src/adapter.ts
+++ src/adapter.ts
@@ -3,6 +3,25 @@ export class Adapter {
   private static DEPENCENCY_TEMPLATE: string = "* [{{package_name}}]({{package_url}}) ({{package_version}})\n";
 
   public static getFileOutput(dependencies: string[][]) {
+    
+  }
 
+  private static getDependenciesOutput(dependencies: string[]): string {
+    let output = "";
+
+    Object.keys(dependencies).forEach(function (key) {
+      let name = key;
+      let version = dependencies[key];
+      let url = "https://www.npmjs.com/package/" + name;
+
+      let line = Adapter.DEPENCENCY_TEMPLATE
+                  .replace("{{package_name}}", name)
+                  .replace("{{package_url}}", url)
+                  .replace("{{package_version}}", version);
+
+      output += line;
+    });
+
+    return output;
   }
 }
