c25b48c3c431366dacabdf002b84209b170d8e13
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-02 18:29:57 +0000

    Create file template

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,3 +1,4 @@
 export class Adapter {
+  private static FILE_TEMPLATE: string = "# Dependencies\n## For users\n{{dependencies}}\n\n## For developers\n{{devDependencies}}";
   private static DEPENCENCY_TEMPLATE: string = "* [{{package_name}}]({{package_url}}) ({{package_version}})\n";
 }
